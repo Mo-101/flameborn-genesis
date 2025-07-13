aa439ed13ca3c931066366745b0f178d1c8ba071
Mo-101 <akiniobong10@gmail.com>
2025-07-13 08:58:50 +0300

    T

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,4 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("hardhat-celo");
-require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -18,26 +16,5 @@ module.exports = {
     hardhat: {
       allowUnlimitedContractSize: false,
     },
-    alfajores: {
-      url: process.env.CELO_ALFAJORES_RPC_URL || "https://alfajores-forno.celo-testnet.org",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 44787,
-      gasPrice: 0.5 * 10**9
-    },
-    celo: {
-      url: process.env.CELO_MAINNET_RPC_URL || "https://forno.celo.org",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 42220,
-      gasPrice: 0.5 * 10**9
-    }
   },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  }
 };
