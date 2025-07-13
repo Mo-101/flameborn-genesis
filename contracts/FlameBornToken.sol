1646e8ee9911121ca31b0bac875bf5bcf64247c6
Mo-101 <akiniobong10@gmail.com>
2025-07-13 03:49:01 +0300

    R

--- contracts/FlameBornToken.sol
+++ contracts/FlameBornToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity =0.8.24;
+pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
-import {IERC20Metadata, IERC20Metadata} from "@openzeppelin/contracts/contracts/data/IERC20Metadata.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 
 /**
